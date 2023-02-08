4fc81273c10601b15ad787196217074e986bec1b
HuuHoa95 <nhintroihoihan@gmail.com>
2023-02-08 21:17:46 +0700

    Test

---
+++ HaUI.java
@@ -0,0 +1,17 @@
+package com.example.javalib;
+
+public class HaUI {
+    public static int x = 5;
+
+    public static void main(String[] args) {
+
+        System.out.println(x);
+        System.out.println("\nAnythings");
+
+        int x = 8;
+        int y = 12;
+
+        Extend test = new Extend();
+        test.getName();
+    }
+}
